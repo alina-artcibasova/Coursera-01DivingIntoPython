aa97547349d1d9706af74b8965f547b3da4be1c8
darthaline <alinartsibasova@gmail.com>
2020-10-15 00:52:12 +0200

    argparse tests

--- Week2/storage.py
+++ Week2/storage.py
@@ -18,8 +18,20 @@
 ## $ python storage.py --key multi_key --val value2
 ## $ python storage.py --key multi_key
 ## value1, value2
+##
+## If value for requested key hasn't been found, then return None
 
-import argparse
-import json
-import os
-import tempfile
+#importing libraries
+import argparse # to parse command line arguments
+import json # to store json files
+import os #
+import tempfile # file creation
+
+parser = argparse.ArgumentParser(description='Storing key-value pairs.')
+parser.add_argument('--key',
+                    help='an integer for the accumulator')
+parser.add_argument('--val',
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args)
